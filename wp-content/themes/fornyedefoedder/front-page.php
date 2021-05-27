d1fcf9b02d0f37b15b859bb9eebe2d5fa5bb1c32
Your Name <you@example.com>
2021-05-27 17:18:12 +0200

    håhåhå

--- wp-content/themes/fornyedefoedder/front-page.php
+++ wp-content/themes/fornyedefoedder/front-page.php
@@ -12,64 +12,65 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-			<div id="section_one">
-				<div id="index_section_one" class="section_one_container_one">
-					<div id="behandlinger">
-					<img src="" alt="">
-						<h2 class="behandlinger">Behandlinger og priser</h2>
-						<p></p>
-						<button>Se mere</button>
-					</div>
-					
-					<div id="fodproblemer">
-					<img src="" alt="">
-						<h2 class="fodproblemer">Info om fødder og fodsygdomme</h2>
-						<p></p>
-						<button>Se mere</button>
-					</div>
-					<div id="om_klinikken">
-					<img src="" alt="">
-						<h2 class="om_klinikken">Om klinikken</h2>
-						<p></p>
-						<button>Se mere</button>
-					</div>
-					
-				</div>
+        <div id="section_one">
+            <div id="index_section_one" class="section_one_container_one">
+                <div id="behandlinger">
+                    <img src="" alt="">
+                    <h2 class="behandlinger">Behandlinger og priser</h2>
+                    <p></p>
+                    <button>Se mere</button>
+                </div>
 
-				<div class="section_one_container_two">
-					<img src="" alt="">
-					<h2 class="statsautoriseret_fodterapeut">Statsautoriseret fodterapeut</h2>
-					<p>Indsæt spændende lang tekst lol</p>
-					<h2 class="statsautoriseret_fodterapeut_two">Hvem har brug for en fodterapeut?</h2>
-					<p>Mere spændende lang tekst - dobbelt lol</p>
-				</div>
+                <div id="fodproblemer">
+                    <img src="" alt="">
+                    <h2 class="fodproblemer">Info om fødder og fodsygdomme</h2>
+                    <p></p>
+                    <button>Se mere</button>
+                </div>
+                <div id="om_klinikken">
+                    <img src="" alt="">
+                    <h2 class="om_klinikken">Om klinikken</h2>
+                    <p></p>
+                    <button>Se mere</button>
+                </div>
 
-			</div>
+            </div>
 
-			<div id="section_two">
-				<div class="section_two_container_one">
-				<h2 class="bliv_kontaktet">Bliv kontaktet</h2>
-					<p>Hvis du har spørgsmål eller gerne vil bestille en tid, kan du bruge nedenstående formular til at blive kontaktet, så vi sammen kan finde frem til den rette behandling.</p>
-					<p>Hvordan vil du kontaktes?</p>
-					<form>
-					<input type="checkbox">
-					<label>Ring mig op</label>
-					<input type="checkbox">
-					<label>SMS</label>
-					<input type="checkbox">
-					<label>E-mail</label>
-				</div>
+            <div class="section_one_container_two">
+                <img src="" alt="">
+                <h2 class="statsautoriseret_fodterapeut">Statsautoriseret fodterapeut</h2>
+                <p>Indsæt spændende lang tekst lol</p>
+                <h2 class="statsautoriseret_fodterapeut_two">Hvem har brug for en fodterapeut?</h2>
+                <p>Mere spændende lang tekst - dobbelt lol</p>
+            </div>
 
-				<div id="section_two_container_two">
-					<input type="tel">
-					<input type="email">
-				</div>
+        </div>
 
-				<div id="section_two_container_three">
-					<textarea name="besked" rows="10"></textares>
+        <div id="section_two">
+            <div class="section_two_container_one">
+                <h2 class="bliv_kontaktet">Bliv kontaktet</h2>
+                <p>Hvis du har spørgsmål eller gerne vil bestille en tid, kan du bruge nedenstående formular til at
+                    blive kontaktet, så vi sammen kan finde frem til den rette behandling.</p>
+                <p>Hvordan vil du kontaktes?</p>
+                <form>
+                    <input type="checkbox">
+                    <label>Ring mig op</label>
+                    <input type="checkbox">
+                    <label>SMS</label>
+                    <input type="checkbox">
+                    <label>E-mail</label>
+            </div>
+
+            <div id="section_two_container_two">
+                <input type="tel">
+                <input type="email">
+            </div>
+
+            <div id="section_two_container_three">
+                <textarea name="besked" rows="10"></textare>
 					<button>Send</button>
 					<p>OBS. Jeg bestræber mig på at vende tilbage inden 24 timer.</p>
 				</div>
