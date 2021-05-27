b64e236456f931ab516d5a80bacbf6cf6075e7f8
Your Name <you@example.com>
2021-05-27 16:22:09 +0200

    test2321

---
+++ wp-content/themes/fornyedefoedder/front-page.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div id="section_one">
+				<div id="index_section_one" class="section_one_container_one">
+					<div id="behandlinger">
+					<img src="" alt="">
+						<h2 class="behandlinger">Behandlinger</h2>
+						<p></p>
+						<button></button>
+					</div>
+					
+					<div id="fodproblemer">
+					<img src="" alt="">
+						<h2 class="fodproblemer">Fodproblemer</h2>
+						<p></p>
+						<button></button>
+					</div>
+					<div id="om_klinikken">
+					<img src="" alt="">
+						<h2 class="om_klinikken">Om klinikken</h2>
+						<p></p>
+						<button></button>
+					</div>
+					
+				</div>
+
+				<div class="section_one_container_two">
+					<img src="" alt="">
+					<h2 class="statsautoriseret_fodterapeut"></h2>
+					<p></p>
+					<h2 class="statsautoriseret_fodterapeut_two"></h2>
+					<p></p>
+				</div>
+
+			</div>
+
+			<div id="section_two">
+				<div class="section_two_container_one">
+				<h2 class="bliv_ringet_op">Bliv ringet op</h2>
+					<p></p>
+					<p></p>
+					<form>
+					<input type="checkbox">
+					<label>Ring mig op</label>
+					<input type="checkbox">
+					<label>SMS</label>
+					<input type="checkbox">
+					<label>E-mail</label>
+				</div>
+
+				<div id="section_two_container_two">
+					<input type="text">
+				</div>
+				</form>	
+
+			</div>
+
+			<div id="section_three">
+				<div class="section_three_container_one">
+
+				</div>
+
+			</div>
+
+			<div id="section_four">
+				<div class="section_four_container_one">
+
+				</div>
+			</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
