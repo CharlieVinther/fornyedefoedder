e2bbb6224dc1e41f1c44ff5885357527f31fd5f1
Your Name <you@example.com>
2021-06-01 19:26:54 +0200

    produkter

--- wp-content/themes/fornyedefoedder/page-produkter.php
+++ wp-content/themes/fornyedefoedder/page-produkter.php
@@ -48,12 +48,107 @@ get_header();
     </main><!-- #main -->
 
     <template>
-                    <article>
-                     <img src="" alt="" class="billede">
-                        <h3 class="produkt"></h3>
-                     </article>
-                  </template>
-                  
+                <article class="produkt_container">
+                    <img src="" alt="" class="produktbillede">
+                    <h3 class="navn"></h3>
+                    <p class="size"></p>
+                </article>
+            </template>
+
+        <script>
+                let produkter;
+                let categories;
+                let filter = "alle";
+
+                console.log("produkter");
+
+                const url = "https://charlievinther.dk/fornyedefoedder/wp-json/wp/v2/produkt?per_page=100";
+                const caturl = "https://charlievinther.dk/fornyedefoedder/wp-json/wp/v2/categories/";
+
+                document.addEventListener("DOMContentLoaded", start);
+
+                function start() {
+                    console.log("start");
+
+                    hentData();
+                }
+
+                function start() {
+                    console.log("start");
+
+                    hentData();
+                }
+
+                async function hentData() {
+                    const respons = await fetch(url);
+                    const catrespons = await fetch(caturl);
+                    produkter = await respons.json();
+                    categories = await catrespons.json();
+                    console.log(produkter);
+                    visProdukter();
+                    opretKnapper();
+                }
+
+                function opretKnapper() {
+                    console.log("opretKnapper")
+
+                    categories.forEach(cat => {
+                        document.querySelector("#filtrering").innerHTML += `<button class="filter" data-produkter="${cat.id}">${cat.name}</button>`
+                    })
+                    addEventListenerToButton();
+                }
+
+                function addEventListenerToButton() {
+                    console.log("button");
+                    document.querySelectorAll("#filtrering button").forEach(knap => {
+                        knap.addEventListener("click", filtrerProdukter);
+                    })
+                }
+
+                function filtrerProdukter() {
+                    filter = this.dataset.produkter;
+                    console.log("filtrerProdukter")
+
+                    visProdukter();
+                }
+
+                function visProdukter() {
+                    console.log("visProdukter");
+
+                    const dest = document.querySelector("#grid_container_podcast");
+                    const temp = document.querySelector("template").content;
+
+                    dest.textContent = "";
+
+                    produkter.forEach(produkt => {
+                        console.log("forEachProdukter");
+
+
+                        if (filter == "alle" || produkter.categories.includes(parseInt(filter))) {
+
+                            console.log("IF");
+
+                            const klon = temp.cloneNode(true);
+
+                            klon.querySelector(".produktbillede").src = produkter.image.guid;
+                            klon.querySelector(".navn").textContent = navn.title.rendered;
+                            klon.querySelector(".size").textContent =  size.produkter;
+                            
+
+                            klon.querySelector(".produkt_container").addEventListener("click", () => location.href = produkter.link);
+
+                            dest.appendChild(klon);
+                            //klon.querySelector("").
+                        }
+                    });
+
+
+                }
+
+
+
+
+         </script>
 
 </div><!-- #primary -->
 
