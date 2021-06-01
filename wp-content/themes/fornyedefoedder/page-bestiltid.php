f48fa4dc78348455c462cd9cf1b528fd5cb8be39
LarsLembke <lars46i6@stud.kea.dk>
2021-06-01 17:43:03 +0200

    sadfghjklæ

--- wp-content/themes/fornyedefoedder/page-bestiltid.php
+++ wp-content/themes/fornyedefoedder/page-bestiltid.php
@@ -17,28 +17,11 @@ get_header();
 
         <div id="main_container">
             <section id="first_section">
-                <div id="behandling_container"></div>
-                <div id="behandling_text_container">
-                    <h2>Tilskud og henvisning</h2>
-                    <p>Patienter har følgende tildkudsmuligheder</p>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <p>Du er velkommen til at <a href="">kontakte</a> mig ved yderligere spørgsmål.</p>
-                </div>
-            </section>
-            <section id="second_section">
-                <h2>Info om fødder og fodsygdomme</h2>
-            </section>
-            <section id="third_section">
-                <div id="formular_img_container"></div>
-                <div id="formular_container">
-                    <div class="section_two_container_one">
-                        <h2 class="bliv_kontaktet">Bliv kontaktet</h2>
-                        <p>Hvis du har spørgsmål eller gerne vil bestille en tid, kan du bruge nedenstående formular til
-                            at blive kontaktet, så vi sammen kan finde frem til den rette behandling.</p>
-                        <p>Hvordan vil du kontaktes?</p>
-                        <form>
+                <article>
+                    <h2></h2>
+                    <p></p>
+                </article>
+                <form>
                             <input type="checkbox">
                             <label>Ring mig op</label>
                             <input type="checkbox">
@@ -56,19 +39,22 @@ get_header();
                         <p>OBS. Jeg bestræber mig på at vende tilbage inden 24 timer.</p>
                     </div>
 
-                    </form>
-                </div>
+                </form>
             </section>
-            <section id="fourth_section">
-                <div id="kontakt_container">
+            <section id="second_section">
+                <div id="map">
+                    <h2></h2>
                     <img src="" alt="">
-                    <p>+45 2014 4866</p>
-                    <img src="" alt="">
-                    <p>fornyedefoedder@gmail.com</p>
-                    <img src="" alt="">
-                    <p>fornyedefødder</p>
                 </div>
+                <article>
+                    <h2></h2>
+                    <p></p>
+                    <h2></h2>
+                    <p></p>
+                    <p></p>
+                </article>
             </section>
+           
 
         </div>
 
--- wp-content/themes/fornyedefoedder/page-klinikken.php
+++ wp-content/themes/fornyedefoedder/page-klinikken.php
@@ -17,56 +17,41 @@ get_header();
 
         <div id="main_container">
             <section id="first_section">
-                <div id="behandling_container"></div>
-                <div id="behandling_text_container">
-                    <h2>Tilskud og henvisning</h2>
-                    <p>Patienter har følgende tildkudsmuligheder</p>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <p>Du er velkommen til at <a href="">kontakte</a> mig ved yderligere spørgsmål.</p>
-                </div>
+                <img src="" alt="">
+                <article>
+                    <h2>Stine Stubtoft</h2>
+                    <h3>Statsautoriseret fodterapeut</h3>
+                    <p></p>
+                    <h3>Fødderne skal ikke isoleres</h3>
+                    <p></p>
+                </article>
             </section>
             <section id="second_section">
-                <h2>Info om fødder og fodsygdomme</h2>
-            </section>
-            <section id="third_section">
-                <div id="formular_img_container"></div>
-                <div id="formular_container">
-                    <div class="section_two_container_one">
-                        <h2 class="bliv_kontaktet">Bliv kontaktet</h2>
-                        <p>Hvis du har spørgsmål eller gerne vil bestille en tid, kan du bruge nedenstående formular til
-                            at blive kontaktet, så vi sammen kan finde frem til den rette behandling.</p>
-                        <p>Hvordan vil du kontaktes?</p>
-                        <form>
-                            <input type="checkbox">
-                            <label>Ring mig op</label>
-                            <input type="checkbox">
-                            <label>SMS</label>
-                            <input type="checkbox">
-                            <label>E-mail</label>
+                <h2>Praktisk info</h2>
+                <div id="ui_container">
+                    <div class="ui_info">
+                        <img src="" alt="">
+                        <p></p>
                     </div>
-                    <div id="section_two_container_two">
-                        <input type="tel">
-                        <input type="email">
+                    <div class="ui_info">
+                        <img src="" alt="">
+                        <p></p>
                     </div>
-                    <div id="section_two_container_three">
-                        <textarea name="besked" rows="10"></textarea>
-                        <button>Send</button>
-                        <p>OBS. Jeg bestræber mig på at vende tilbage inden 24 timer.</p>
+                    <div class="ui_info">
+                        <img src="" alt="">
+                        <p></p>
                     </div>
-
-                    </form>
                 </div>
             </section>
-            <section id="fourth_section">
-                <div id="kontakt_container">
+            <section id="third_section">
+                <div id="findvej_container">
+                    <article>
+                        <h2></h2>
+                        <p></p>
+                        <h3></h3>
+                        <img src="" alt="">
+                    </article>
                     <img src="" alt="">
-                    <p>+45 2014 4866</p>
-                    <img src="" alt="">
-                    <p>fornyedefoedder@gmail.com</p>
-                    <img src="" alt="">
-                    <p>fornyedefødder</p>
                 </div>
             </section>
 
