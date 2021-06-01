1dca71aabe5d5acb928783e6085b0d957990acbe
Your Name <you@example.com>
2021-06-01 17:56:55 +0200

    kategori

--- wp-content/themes/fornyedefoedder/page-produkter.php
+++ wp-content/themes/fornyedefoedder/page-produkter.php
@@ -26,13 +26,16 @@ get_header();
             <section id="second_section">
                 <h2 class="produkter">Produkter</h2>
                 <nav class="filtrering">
-                 <button data-produkter="alle" class="valgt">Alle</button>
-                 <button data-produkter="haard hud" class="haardhud">Hård hud</button>
-                 <button data-produkter="neglebehandling" class="neglebehandling">Neglebehandling</button>
-                 <button data-produkter="saarbehandling" class="saarbehandling">Sårbehandling</button>
-                 <button data-produkter="saaler" class="saaler">Såler</button>
+                 <button data-produkter="alle" class="valgt">Alle produkter</button>
+                 <button data-produkter="eksem-og-saar" class="eksem-og-saar">Eksem og sår</button>
+                 <button data-produkter="haelerevner" class="haelerevner">Hælerevner</button>
+                 <button data-produkter="svamp" class="svamp">Problemer med svamp</button>
+                 <button data-produkter="psoriasis" class="psoriasis">Psoriasis</button>
+                 <button data-produkter="sportsudoevere" class="sportsudoevere">Til sportsudøvere</button>
+                 <button data-produkter="sved-og-lugt" class="sved-og-lugt">Sved og lugt</button>
+                 <button data-produkter="toerhud" class="toerhud">Tør hud</button>
+                 <button data-produkter="tyndhud" class="tyndhud">Tynd hud</button>
              </nav>
-             <h2 class="alle">Alle</h2>
             </section>
 
 
