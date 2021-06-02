d2db13770fec81bb58c8fc26dbe4c54885d8975e
Your Name <you@example.com>
2021-06-02 11:16:21 +0200

    dfssd

--- wp-content/themes/fornyedefoedder/single-produkt.php
+++ wp-content/themes/fornyedefoedder/single-produkt.php
@@ -75,7 +75,7 @@ get_header();
 
             function visProdukt() {
                 console.log("visProdukt");
-                document.querySelector(".billede").src = produkt.image.guid;
+                //document.querySelector(".billede").src = produkt.image.guid;
                 document.querySelector(".navn").innerHTML = produkt.navn;
                 document.querySelector(".beskrivelse").textContent = produkt.beskrivelse;
                 document.querySelector(".brandlogo").src = produkt.image.guid;
