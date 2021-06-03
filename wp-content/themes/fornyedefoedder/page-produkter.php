6fb75f294a26a95cc060de58baaf8bb76deb5766
Your Name <you@example.com>
2021-06-03 11:16:38 +0200

    sdfsdfs

--- wp-content/themes/fornyedefoedder/page-produkter.php
+++ wp-content/themes/fornyedefoedder/page-produkter.php
@@ -42,8 +42,8 @@ get_header();
                 <p class="billedetekst"></p>
                 </section>
 
-                <section id="fourth_section" class="grid"> 
-                <div id="grid_container"></div>
+                <section id="section_four" class="grid"> 
+                <div class="container">
                 <div class="col">
                     <h2 class="info_om_fodsygdomme">Info om fodsygdomme</h2>
                     <p class="tekst_om_fodsygdomme">Mange patienter er usikre omkring deres fødder og får derfor ikke set en 
@@ -53,7 +53,7 @@ Jeg har derfor samlet noget information omkring de spørgsmål du evt. kunne hav
 – information generelt om fødder og fodsygdomme.</p>
                 <button class="se_mere">Se mere</button>
                 </div>
-                <img src="http://charlievinther.dk/fornyedefoedder/wp-content/uploads/2021/06/infografik.png" alt="" class="infografik">
+                    <img src="http://charlievinther.dk/fornyedefoedder/wp-content/uploads/2021/06/infografik.png" alt="" class="infografik">
                 </div>
                 </section>
 
