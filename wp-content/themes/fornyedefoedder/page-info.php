890c7183ca0bb15ea27f621e54e8356eef469133
LarsLembke <lars46i6@stud.kea.dk>
2021-06-03 13:21:54 +0200

    bcsjbhcsd

--- wp-content/themes/fornyedefoedder/page-info.php
+++ wp-content/themes/fornyedefoedder/page-info.php
@@ -17,24 +17,72 @@ get_header();
 
         <div id="main_container">
             <section id="first_section">
-                <nav>
-                    <button data-info="Alle"></button>
-                </nav>
-            </section>
-            <section id="second_section">
-                
+                <div class="container">
+                    <div id="info_loop">
+                    </div>
+                </div>
             </section>
         </div>
 
     </main><!-- #main -->
     <template>
-        <article></article>
+        <article>
+            <img src="" alt="" class="illu">
+            <h3 class="titel"></h3>
+            <p class="kilde"></p>
+            <a href="" class="kildelink">
+                <button>Læs mere</button>
+            </a>
+        </article>
     </template>
 
 
     <script>
         let info;
-        let url;
+        const url = "";
+
+        console.log("info")
+
+        document.addEventListener("DOMContentLoaded", start);
+
+        function start() {
+            console.log("start");
+            
+            hentData();
+        }
+    
+        async function hentData () {
+            const respons = await fetch(url);
+            info = await respons.json();
+
+            console.log(info);
+
+            visInfo();
+        }
+
+        visInfo() {
+            console.log("visInfo");
+
+            const temp = document.querySelector("template").content;
+            const dest = document.querySelector("#info_loop");
+
+            info.forEach(e => {
+                console.log("forEach");
+
+                const klon = temp.cloneNode(true);
+
+                klon.querySelector(".titel").innerHTML = e.titel;
+                klon.querySelector("img").src = e.illu.guid;
+                klon.querySelector(".kilde").innerHTML = e.kilde;
+                klon.querySelector(".kildelink").href = e.kildelink;
+                
+                
+
+                dest.appendChild(klon);
+
+            });
+
+        }
     
     
     </script>
